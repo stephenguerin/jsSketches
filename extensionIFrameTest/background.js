bbed17549d2f7316a4eeca3b82ffda93e21a8656
cody s <cody@simtable.com>
2020-06-24 14:06:33 -0600

    extension

---
+++ extensionIFrameTest/background.js
@@ -0,0 +1,30 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+"use strict";
+
+chrome.runtime.onInstalled.addListener(function () {
+  console.log("background on installed");
+  var a = document.createElement("iframe");
+  a.src = "https://dev.simtable.com/observer/alpha/map";
+  a.width = 500;
+  a.height = 500;
+  document.body.appendChild(a);
+  chrome.storage.sync.set({ color: "#3aa757" }, function () {
+    console.log("The color is green.");
+  });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "developer.chrome.com" },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ]);
+    console.log("background onpagechanged");
+  });
+});
